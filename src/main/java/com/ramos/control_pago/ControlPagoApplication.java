0ad89b2651bc2df73dc21c3b09bb6c59e9f2b273
eduRamos1620 <eduardoramosrosales16@gmail.com>
2026-05-22 16:12:28 -0600

    Se crea proyecto Spring Boot, con BD Postgres en contenedor Docker

---
+++ src/main/java/com/ramos/control_pago/ControlPagoApplication.java
@@ -0,0 +1,13 @@
+package com.ramos.control_pago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlPagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlPagoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramos/control_pago/ControlPagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramos.control_pago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlPagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
